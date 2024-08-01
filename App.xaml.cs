c2618bc3d8bb7ec75b8764844f6e7e9d86c065f2
ZohebKhan007 <84551540+ZohebKhan007@users.noreply.github.com>
2024-08-01 22:08:10 +1200

    Second Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using TaskPlanner.MVVM.Views;
+
+namespace TaskPlanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainView());
+        }
+    }
+}
