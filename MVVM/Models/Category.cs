53ffe7d7b69469fcc265ebdadbc3939ea2f8582b
ZohebKhan007 <84551540+ZohebKhan007@users.noreply.github.com>
2024-08-01 22:11:53 +1200

    Adding Files into Models Folder

---
+++ MVVM/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int PendingTasks { get; set; }
+        public float Percentage { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ MVVM/Models/MyTask.cs
@@ -0,0 +1,18 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MyTask
+    {
+        public string TaskName { get; set; } = string.Empty;
+        public bool Completed { get; set; }
+        public int CategoryID { get; set; }
+        public string TaskColor { get; set; } = string.Empty;
+    }
+}
